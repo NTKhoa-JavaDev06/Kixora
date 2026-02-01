b7882ee74c3959643c758f0670067d86dae53581
Nguyen Tuan Khoa <nguyenkhanh7510@gmail.com>
2026-02-01 14:17:04 +0700

    Up File

---
+++ src/main/java/poly/com/asm_kixora/AsmKixoraApplication.java
@@ -0,0 +1,13 @@
+package poly.com.asm_kixora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmKixoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmKixoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poly/com/asm_kixora/AsmKixoraApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.com.asm_kixora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmKixoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
